de024ef9ec4b0661c95f0019fc11e5118dacb668
moong <kmg8498@naver.com>
2018-09-06 20:15:31 +0900

    add maximam except

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main()
 	printf("Input Dan : ");
 	scanf("%d", &dan);
 
-	if(dan < 2)
+	if(dan < 2 || dan > 9)
 	{
-		printf("Miniman dan is 2\n");
+		printf("Miniman dan is 2 and Maximan dan is 9\n");
 		return -1;
 	}
 
