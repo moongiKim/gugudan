7d0fe10ef5f7adbcf2a4834382fb63d4e0c90aab
moong <kmg8498@naver.com>
2018-09-06 20:12:47 +0900

    add miniman except

--- main.c
+++ main.c
@@ -7,6 +7,12 @@ int main()
 	printf("Input Dan : ");
 	scanf("%d", &dan);
 
+	if(dan < 2)
+	{
+		printf("Miniman dan is 2\n");
+		return -1;
+	}
+
 	for(i=1; i<9; i++)
 	{
 		printf("[%d]*[%d] = [%d]\n",dan, i, dan*i);
